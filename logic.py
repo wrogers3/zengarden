474217f4d897b1c14947ef0341218ed2322b9a46
Warren Rogers <z5363427@ad.unsw.edu.au>
2023-06-16 21:21:47 +1000

    added other function files with stubs

---
+++ logic.py
@@ -0,0 +1,9 @@
+def getAction([lifeCycle, sunReqs, waterPreference]):
+    # based on these parameters, make a daily cycle for how the following hardware must work
+    # mister
+    # night shade
+    # LED simulations
+    # Humidity intake + thresholds / limits
+    # how long can lid be taken off
+    # 
+    return null;
---
+++ main.py
@@ -0,0 +1,6 @@
+# main program to be run for the greenhouse
+# will run only until new plant is on
+
+# has timers for when lid is opened
+# handles humidity
+
