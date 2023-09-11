0d7c52af419922998694aba9316424fa7393c5d0
Warren Rogers <warrenrogers127@gmail.com>
2023-09-11 19:59:26 +1000

    removed bloat

--- logic.py
+++
@@ -1,9 +0,0 @@
-def getAction([lifeCycle, sunReqs, waterPreference]):
-    # based on these parameters, make a daily cycle for how the following hardware must work
-    # mister
-    # night shade
-    # LED simulations
-    # Humidity intake + thresholds / limits
-    # how long can lid be taken off
-    # 
-    return null;
